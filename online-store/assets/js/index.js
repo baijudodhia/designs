1fd6399007392ae456ce8531d0aa939daecb3bad
Baiju Dodhia <51258945+baijudodhia@users.noreply.github.com>
2020-12-29 01:21:39 +0530

    Homepage design completed #OnlineStoreDesign

---
+++ online-store/assets/js/index.js
@@ -0,0 +1,54 @@
+function FilterCategory(category) {
+    console.log(category);
+    let products = document.getElementById("products").children;
+    let btn_filter_all = document.getElementById("btn-filter-all");
+    let btn_filter_phone = document.getElementById("btn-filter-phone");
+    let btn_filter_laptop = document.getElementById("btn-filter-laptop");
+    let btn_filter_desktop = document.getElementById("btn-filter-desktop");
+    if (category == 'all') {
+        btn_filter_all.classList.remove('btn-filter-active');
+        btn_filter_phone.classList.remove('btn-filter-active');
+        btn_filter_laptop.classList.remove('btn-filter-active');
+        btn_filter_desktop.classList.remove('btn-filter-active');
+        btn_filter_all.classList += ' btn-filter-active';
+        Array.from(products).forEach((ele) => {
+            ele.classList.remove("product_visible");
+            ele.classList.remove("product_hidden");
+            ele.className += " product_visible ";
+            console.log(ele.className + " - show");
+        });
+    } else {
+        if (category == 'phone') {
+            btn_filter_all.classList.remove('btn-filter-active');
+            btn_filter_phone.classList.remove('btn-filter-active');
+            btn_filter_laptop.classList.remove('btn-filter-active');
+            btn_filter_desktop.classList.remove('btn-filter-active');
+            btn_filter_phone.classList += ' btn-filter-active';
+        } else if (category == 'laptop') {
+            btn_filter_all.classList.remove('btn-filter-active');
+            btn_filter_phone.classList.remove('btn-filter-active');
+            btn_filter_laptop.classList.remove('btn-filter-active');
+            btn_filter_desktop.classList.remove('btn-filter-active');
+            btn_filter_laptop.classList += ' btn-filter-active';
+        } else if (category == 'desktop') {
+            btn_filter_all.classList.remove('btn-filter-active');
+            btn_filter_phone.classList.remove('btn-filter-active');
+            btn_filter_laptop.classList.remove('btn-filter-active');
+            btn_filter_desktop.classList.remove('btn-filter-active');
+            btn_filter_desktop.classList += ' btn-filter-active';
+        }
+        Array.from(products).forEach((ele) => {
+            if (!ele.classList.contains(category)) {
+                ele.classList.remove("product_visible");
+                ele.classList.remove("product_hidden");
+                ele.className += " product_hidden ";
+                console.log(ele.className + " - hide");
+            } else {
+                ele.classList.remove("product_visible");
+                ele.classList.remove("product_hidden");
+                ele.className += " product_visible ";
+                console.log(ele.className + " - show");
+            }
+        });
+    }
+}
